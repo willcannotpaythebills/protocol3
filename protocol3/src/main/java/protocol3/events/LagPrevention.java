d7a3d8476ea20d80d07bde61ba420cecb6e95a00
Infer4Y <46350196+Infer4Y@users.noreply.github.com>
2021-03-10 18:17:35 -0500

    Optimizing code. This may not look like much yet, but I will try for more ways to reduce the size.

--- protocol3/src/main/java/protocol3/events/LagPrevention.java
+++ protocol3/src/main/java/protocol3/events/LagPrevention.java
@@ -1,13 +1,18 @@
 package protocol3.events;
 
+import com.google.common.base.Predicate;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Wither;
+import org.bukkit.entity.WitherSkull;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntitySpawnEvent;
 
 import protocol3.backend.Config;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class LagPrevention implements Listener
 {
@@ -31,46 +36,21 @@ public class LagPrevention implements Listener
 		}
 	}
 
-	public static int getWithers()
-	{
+	public static int getWithers() {
+		String[] worldTypes = new String[]{"world", "world_nether", "world_the_end"};
 		int toRet = 0;
 		int witherLimit = Integer.parseInt(Config.getValue("wither.limit"));
-		for (Entity e : Bukkit.getWorld("world").getEntities())
-		{
-			if (e.getType().equals(EntityType.WITHER))
-			{
-				toRet++;
-				if (toRet > witherLimit)
-				{
-					toRet--;
-					Wither w = (Wither) e;
-					w.setHealth(0);
-				}
-			}
-		}
-		for (Entity e : Bukkit.getWorld("world_nether").getEntities())
-		{
-			if (e.getType().equals(EntityType.WITHER))
-			{
-				toRet++;
-				if (toRet > witherLimit)
-				{
-					toRet--;
-					Wither w = (Wither) e;
-					w.setHealth(0);
-				}
-			}
-		}
-		for (Entity e : Bukkit.getWorld("world_the_end").getEntities())
-		{
-			if (e.getType().equals(EntityType.WITHER))
-			{
-				toRet++;
-				if (toRet > witherLimit)
-				{
-					toRet--;
-					Wither w = (Wither) e;
-					w.setHealth(0);
+		List<Entity> entities;
+		for (String worldType : worldTypes) {
+			entities = Bukkit.getWorld(worldType).getEntities().stream().filter(e -> (e instanceof Wither)).collect(Collectors.toList());
+			for (Entity e : entities) {
+				if (e.getType().equals(EntityType.WITHER) && e.getCustomName() == null) {
+					toRet++;
+					if (toRet > witherLimit) {
+						toRet--;
+						Wither w = (Wither) e;
+						w.setHealth(0);
+					}
 				}
 			}
 		}
