74f169806285a62cdf8792351c1be722246d911d
ultra <mark.ruben7331@protonmail.com>
2021-03-15 12:31:54 -0700

    use exponentially weighted moving average to calculate TPS

--- protocol3/src/main/java/protocol3/backend/LagProcessor.java
+++ protocol3/src/main/java/protocol3/backend/LagProcessor.java
@@ -1,41 +1,28 @@
 package protocol3.backend;
 
+/* Calculate the exponentially weighted moving average of TPS */
 public class LagProcessor implements Runnable {
-	public static int TICK_COUNT = 0;
-	public static long[] TICKS = new long[600];
-	public static long LAST_TICK = 0L;
-
-	public static boolean sendFlag = true;
-	public static int safe = 0;
+	public static long LAST_TICK_TS = -1;
+	public static double AVERAGE_TICK = 50.0; // 20 TPS = 50 ms tick len
+	public static double ALPHA = 0.01; // TODO make this a config option
 
 	public static double getTPS() {
-		return getTPS(100);
-	}
-
-	public static double getTPS(int ticks) {
-		if (TICK_COUNT < ticks) return 20.0D;
-
-		int target = (TICK_COUNT - 1 - ticks) % TICKS.length;
-		long elapsed = System.currentTimeMillis() - TICKS[target];
-		double finalVal = ticks / (elapsed / 1000.0D);
-
-		if (finalVal > 20) finalVal = 20;
-
-		return finalVal;
-	}
-
-	public static long getElapsed(int tickID) {
-		if (TICK_COUNT - tickID >= TICKS.length);
-
-		long time = TICKS[(tickID % TICKS.length)];
-
-		return System.currentTimeMillis() - time;
+		return 1000.0 / AVERAGE_TICK; // one second / tick len == tps
 	}
 
 	@Override
 	public void run() {
-		TICKS[(TICK_COUNT % TICKS.length)] = System.currentTimeMillis();
+		long now = System.currentTimeMillis();
 
-		TICK_COUNT += 1;
+		if (LAST_TICK_TS < 0)
+			LAST_TICK_TS = now - 50; // exactly one tick ago
+
+		// at 20 TPS tick len can be anywhere from 48 - 52 ms
+		long tick_len = now - LAST_TICK_TS;
+		if (tick_len <= 52)
+			tick_len = 50;
+
+		AVERAGE_TICK = ALPHA * tick_len + (1 - ALPHA) * AVERAGE_TICK;
+		LAST_TICK_TS = now;
 	}
 }
